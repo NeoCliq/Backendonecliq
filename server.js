f04debb75ad487b3542a630f27787f1301c41bdd
NeoCliq <neocliq@gmail.com>
2025-04-10 09:20:24 -0300

    Erros e mais erros

--- server.js
+++ server.js
@@ -221,7 +221,7 @@ app.post("/entidade", async (req, res) => {
     // 3. Adiciona o tipo na tabela 'users'
     const { error: userInsertError } = await supabase.from("users").insert([
       {
-        user_id,
+        id,
         tipo,
       },
     ]);
